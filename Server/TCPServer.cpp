18c810a7d726c2b2d214f507e2afc534bb921d89
JRazek <jakub.jakubecky123@gmail.com>
2021-07-19 17:57:22 +0200

    ok :)

--- Server/TCPServer.cpp
+++ Server/TCPServer.cpp
@@ -37,13 +37,14 @@ TCPServer::TCPServer(u_short port, u_short BUFFER_SIZE):port(port), BUFFER_SIZE(
     }
 }
 
-u_short TCPServer::listen() {
+u_short TCPServer::listen() const {
     if (::listen(server_fd, 3) < 0){
         perror("listen");
         return -1;
     }
     return 0;
 }
+
 u_short TCPServer::accept() {
     int addrLen = sizeof(address);
     if ((new_socket = ::accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrLen))<0){
