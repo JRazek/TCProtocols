20536378692bc06947f023b9e0e89482ff79f589
JRazek <jakub.jakubecky123@gmail.com>
2021-07-21 20:46:16 +0200

    some documentation for client. Multiple connections to do for server instance

--- Client/TCPClient.h
+++ Client/TCPClient.h
@@ -15,15 +15,47 @@ private:
     int sock;
     sockaddr_in serv_addr;
 
+    /**
+     * sends raw data to server
+     * for use only by internal methods as it may corrupt the connection
+     * @return status code - 0 if success
+     */
     int sendDataRaw(const byte *data, u_int64_t dataSize);
 
 public:
     TCPClient(const char *addr, u_short port);
+
+
+    /**
+     * connects to specified in the constructor ip and port
+     * @return status code - 0 if success
+     */
     int connect();
 
+
+    /**
+     * sends header containing expected packets
+     * @return status code - 0 if success
+     */
     int sendPacketsMetaData(u_short packetsCount);
+
+
+    /**
+     * sends encoded packet to server
+     * @return status code - 0 if success
+     */
     int sendPacket(const std::vector<byte> &data);
+
+    /**
+     * sends encoded packet to server
+     * @return status code - 0 if success
+     */
     int sendPacket(const byte *bytes, u_int64_t size);
+
+
+    /**
+     * closes the connection with server
+     */
     void connClose();
 };
 
